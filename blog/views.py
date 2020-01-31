d8d4b2c8b3b5e7dadbdb65373ff41bd51b60711e
Luke Lorenger <dirtydeadtv@gmail.com>
2020-01-30 23:50:41 -0500

    meta tags implemented into base.html, bootstrap is functioning

--- blog/views.py
+++ blog/views.py
@@ -5,14 +5,33 @@ from django.shortcuts import render
 # https://youtu.be/a48xeeo5Vnk?t=919 explains routing of URLs
 # blog -> templates ->blog ->template.html
 
+# Passable "posts", using as dummy data, this is a list of dictionaries
+posts = [
+	{
+		'author': 'CoreyMS',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'January 30, 2020'
+	},
+	{
+		'author': 'Jane Doe',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': 'January 30, 2020'
+	}
+]
+
 # Home function, handling traffic from homepage, taking in requests arguements
 # Returning what we want the user to see when they are sent to this route
 # Still returns HttpsResponse in background/This is passing info into our html.template
 def home(request):
-	return render(request, 'blog/home.html')
+	context = { # Dictionary
+		'posts': posts # Key, makes dummy data accessible from within home.html template
+	}
+	return render(request, 'blog/home.html', context)
 
 # About function, Handles logic for about page, taking in request arguements
 # Returning what we want the user to see when they are sent to this route
 # Still returns HttpsResponse in background/This is passing info into our html.template
 def about(request):
-	return render(request, 'blog/about.html')
+	return render(request, 'blog/about.html', {'title': 'About'})
