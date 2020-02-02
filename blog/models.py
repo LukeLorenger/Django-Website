bc109c3850c318ec14c49f954584c63ebf7fa91a
Luke Lorenger <dirtydeadtv@gmail.com>
2020-02-02 10:42:49 -0500

    Database and migrations, dummy data no longer needed

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+# Think about the things you want save to database
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone # Import current time
+from django.contrib.auth.models import User # post, user will have relationship, users will auth posts, one to many relationship
+
+# Models
+# Each class will be its own table in database
+class Post(models.Model): # inheritence from models
+	title = models.CharField(max_length=100)
+	content = models.TextField() # Text field is unrestricted text
+	date_posted = models.DateTimeField(default=timezone.now) # pass in actual function as defualt value
+	author = models.ForeignKey(User, on_delete=models.CASCADE) # If user deleted, then we will delete post as well.
--- blog/views.py
+++ blog/views.py
@@ -1,32 +1,18 @@
 from django.shortcuts import render
+from .models import Post # the . in fron of models means from model file in current package
 
 # Self note, it is a good thing to pass the URL around in this manner
 # Helps with future production and routing purposes for steering users to proper location
 # https://youtu.be/a48xeeo5Vnk?t=919 explains routing of URLs
 # blog -> templates ->blog ->template.html
 
-# Passable "posts", using as dummy data, this is a list of dictionaries
-posts = [
-	{
-		'author': 'CoreyMS',
-		'title': 'Blog Post 1',
-		'content': 'First post content',
-		'date_posted': 'January 30, 2020'
-	},
-	{
-		'author': 'Jane Doe',
-		'title': 'Blog Post 2',
-		'content': 'Second post content',
-		'date_posted': 'January 30, 2020'
-	}
-]
 
 # Home function, handling traffic from homepage, taking in requests arguements
 # Returning what we want the user to see when they are sent to this route
 # Still returns HttpsResponse in background/This is passing info into our html.template
 def home(request):
 	context = { # Dictionary
-		'posts': posts # Key, makes dummy data accessible from within home.html template
+		'posts': Post.objects.all() # 
 	}
 	return render(request, 'blog/home.html', context)
 
