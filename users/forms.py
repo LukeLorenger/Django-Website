7e3064c9f8016055844c10b1cfd93f8e2542562e
Luke Lorenger <dirtydeadtv@gmail.com>
2020-02-06 23:22:05 -0500

    UserRegisterForm created and replaced UserCreationForm, fully functioning form

--- users/forms.py
+++ users/forms.py
@@ -2,3 +2,14 @@ from django import forms # importing forms from django
 from django.contrib.auth.models import User # import user model
 from django.contrib.auth.forms import UserCreationForm # import user creation form
 
+# new class/form that inherits from UserCreationForm
+class UserRegisterForm(UserCreationForm):
+	email = forms.EmailField()# adding fields
+
+	# Gives us a nest of namespace for configurations, keeps configs in one place
+	class Meta:
+		model = User # model to be affected
+		fields = ['username', 'email', 'password1', 'password2'] # fields that we want in the form, and in what order
+
+
+# Completed form that inherits from UserCreationForm
--- users/views.py
+++ users/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm # New user form
 from django.contrib import messages # What type of message we would like to add
+from .forms import UserRegisterForm # importing the created UserRegisterForm with email field
 
 # register view FUNCTION
 def register(request):
 	if request.method == 'POST': # If we get post request, we will validate form data
-		form = UserCreationForm(request.POST)
+		form = UserRegisterForm(request.POST)
 		if form.is_valid():
 			form.save() # Save User
 			username = form.cleaned_data.get('username') # If valid, grab username
@@ -13,7 +13,7 @@ def register(request):
 			return redirect('blog-home') # name we gave to URL pattern for blog home page
 
 	else: # if not, display blank form
-		form = UserCreationForm() # Instance of the form
+		form = UserRegisterForm() # Instance of the form
 	return render(request, 'users/register.html', {'form': form}) # render template that uses this form, uses instance
 
 
