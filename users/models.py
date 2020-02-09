f043b36c245428c6c2f5cbf2a3c82c540f3054d4
Luke Lorenger <dirtydeadtv@gmail.com>
2020-02-09 10:59:21 -0500

    User Profile able to be created, profile pic is uploadable

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User # extending existing user model
 
-# Create your models here.
+# New Model, inherit from models.Model
+class Profile(models.Model):
+	#OnetoOne relationship with existing user model
+	user = models.OneToOneField(User, on_delete=models.CASCADE) # personal user data to be deleted w/ user
+	image = models.ImageField(default='default.jpg', upload_to='profile_pics') # Image field
+
+	# dunder str method
+	def __str__(self): # takes self as args, self is instance 
+		return f'{self.user.username} Profile' # everytime profile is printed, it will print username
