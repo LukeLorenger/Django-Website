dc284bbdf43d122b161f2dc08e311284d5defadf
Luke Lorenger <dirtydeadtv@gmail.com>
2020-02-10 10:11:39 -0500

    PostListView class created, importing to urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from .views import PostListView # importing PostListView class from views.py
 from . import views # Imports views.py module
 
 urlpatterns = [
-    path('', views.home, name='blog-home'), # Path for home route
+    path('', PostListView.as_view(), name='blog-home'), # Path for home route
     # 3. program sees empty string, so it looks for pattern to match empty route^
     # 4. empty string pattern matches, will be handled by the function views.home^
     path('about/', views.about, name='blog-about'), # Path for about route
 ]
+
+# <app>/<model>_<viewtype>.html Looking for a template with a same condition
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView # Importing ListView
 from .models import Post # the . in fron of models means from model file in current package
 
 # Self note, it is a good thing to pass the URL around in this manner
+# We have a home page that gathers all post objects, passes them to home.html
 # Helps with future production and routing purposes for steering users to proper location
 # https://youtu.be/a48xeeo5Vnk?t=919 explains routing of URLs
 # blog -> templates ->blog ->template.html
 
 
-# Home function, handling traffic from homepage, taking in requests arguements
+# Home view function, handling traffic from homepage, taking in requests arguements
 # Returning what we want the user to see when they are sent to this route
 # Still returns HttpsResponse in background/This is passing info into our html.template
 def home(request):
@@ -16,7 +18,11 @@ def home(request):
 	}
 	return render(request, 'blog/home.html', context)
 
-# About function, Handles logic for about page, taking in request arguements
+# new class for class based views
+class PostListView(ListView):
+	model = Post
+
+# About view function, Handles logic for about page, taking in request arguements
 # Returning what we want the user to see when they are sent to this route
 # Still returns HttpsResponse in background/This is passing info into our html.template
 def about(request):
