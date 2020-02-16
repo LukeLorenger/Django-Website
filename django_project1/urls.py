735ee237c42a0cb16d789a494840779c6c34fb44
Luke Lorenger <dirtydeadtv@gmail.com>
2020-02-16 00:48:07 -0500

    password reset functionality almost complete

--- django_project1/urls.py
+++ django_project1/urls.py
@@ -26,6 +26,21 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'), # path to register view
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'), # path to log in view
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'), # path to log out view
+    path('password-reset/',
+    	 auth_views.PasswordResetView.as_view(
+    	 	 template_name='users/password_reset.html'
+    	 ),
+    	 name='password_reset'), # route that provides form for password to fill out   
+    path('password-reset/done/',
+    	 auth_views.PasswordResetDoneView.as_view(
+    	 	 template_name='users/password_reset_done.html'
+    	 ),
+    	 name='password_reset_done'), # route that provides form for password to fill out   
+    path('password-reset-confirm/<uidb64>/<token>', # password reset confirm path
+    	 auth_views.PasswordResetConfirmView.as_view(
+    	 	 template_name='users/password_reset_confirm.html'
+    	 ),
+    	 name='password_reset_confirm'), # route that provides form for password to fill out 
     path('', include('blog.urls')), # path to home
 ]
     # 1. When you navigate to /blog, program checks for a match
